6de264e7579e154bb1763a1df1fb79ad180823d7
Fran <francisco.chiarino@gmail.com>
2021-12-24 12:38:48 +0100

    Add user validation and form errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :blog_name,  length: { minimum: 4 }
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates :email,      presence: true
+  validates :password,   length: { in: 6..20 }
 end
