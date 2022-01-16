ee4a92fab087f5f41e1db52a150951a3fecfd1b1
Fran <francisco.chiarino@gmail.com>
2022-01-16 11:49:19 +0100

    Implement Action Text :memo:

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   friendly_id :title, use: :slugged
 
   belongs_to :user
+  has_rich_text :body
 
   validates :title, length: { in: 4..20 }
   validates :body,  length: { in: 6..10000 }
